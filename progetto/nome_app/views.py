b73c9b743ebd484f2787c9ac35978af495ee5286
manuel dellagala <manuel.dellagala@gmail.com>
2021-11-04 21:54:04 +0100

    ok

--- progetto/nome_app/views.py
+++ progetto/nome_app/views.py
@@ -4,10 +4,60 @@ from django.http import HttpResponse
 
 
 def view_a(request):
-    return HttpResponse("<h1>home<h1>")
+    return HttpResponse("""
+    <h1>home<h1>
+
+    <ul>
+        <li>
+        <a href="/">home<a>
+        </li>
+
+        <li>
+        <a href="nome_app/view_b">vista b<a>
+        </li>
+
+        <li>
+        <a href="nome_app/view_c">vista c<a>
+        </li>
+    </ul
+    
+    """)
 
 def view_b(request):
-    return HttpResponse("<h1>view b<h1>")
+    return HttpResponse("""
+    <h1>view b<h1>
+
+    <ul>
+        <li>
+        <a href="/">home<a>
+        </li>
+
+        <li>
+        <a href="view_b">vista b<a>
+        </li>
+
+        <li>
+        <a href="view_c">vista c<a>
+        </li>
+    </ul
+    
+    """)
 
 def view_c(request):
-    return HttpResponse("<h1>view c<h1>")
+    return HttpResponse("""
+    <h1>view c<h1>
+    <ul>
+        <li>
+        <a href="/">home<a>
+        </li>
+
+        <li>
+        <a href="../view_b">vista b<a>
+        </li>
+
+        <li>
+        <a href="../view_c">vista c<a>
+        </li>
+    </ul
+    
+    """)
